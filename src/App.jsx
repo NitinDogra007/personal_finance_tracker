cfca3e37189b87ba4a5b535516d4922f9e454344
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-04-07 15:10:32 +1000

    fix option values

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,57 @@
 // chart integration: https://github.com/reactchartjs/react-chartjs-2
 // import { useState } from "react";
-import { useState } from "react";
-import ButtonComponent from "./components/ButtonComponent.jsx";
-import Chart from "./components/ChartContainer.jsx";
-import ModalComponent from "./components/ModalComponent.jsx";
-import NavBar from "./components/NavBar";
-import TableComponent from "./components/TableComponent.jsx";
-import "./styles/global.css";
+import { useState } from 'react';
+import ButtonComponent from './components/ButtonComponent.jsx';
+import Chart from './components/ChartContainer.jsx';
+import ModalComponent from './components/ModalComponent.jsx';
+import NavBar from './components/NavBar';
+import TableComponent from './components/TableComponent.jsx';
+import './styles/global.css';
 
 function App() {
-  const [data, setData] = useState([
-    {
-      name: "Salary",
-      type: "Income",
-      category: "Job",
-      date: "2025-04-01",
-    },
-    {
-      name: "Groceries",
-      type: "Expense",
-      category: "Food",
-      date: "2025-04-02",
-    },
-    // Add more entries here
-  ]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [data, setData] = useState([
+		{
+			name: 'Salary',
+			type: 'Income',
+			amount: '2000',
+			category: 'Job',
+			date: '2025-04-01',
+		},
+		{
+			name: 'Groceries',
+			type: 'Expense',
+			amount: '1000',
+			category: 'Food',
+			date: '2025-04-02',
+		},
+		// Add more entries here
+	]);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  function openModal() {
-    setIsModalOpen(true);
-  }
+	function openModal() {
+		setIsModalOpen(true);
+	}
 
-  function closeModal() {
-    setIsModalOpen(false);
-  }
+	function closeModal() {
+		setIsModalOpen(false);
+	}
 
-  // Function to add data to the table
-  function addData(formData) {
-    setData((prevData) => [...prevData, formData]);
-  }
+	// Function to add data to the table
+	function addData(formData) {
+		setData((prevData) => [...prevData, formData]);
+	}
 
-  return (
-    <>
-      <NavBar />
-      <Chart />
-      <ButtonComponent title="Add Transaction" onClick={openModal} />
-      <TableComponent data={data} />
-      {isModalOpen && (
-        <ModalComponent closeModal={closeModal} addData={addData} />
-      )}
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<Chart />
+			<ButtonComponent title="Add Transaction" onClick={openModal} />
+			<TableComponent data={data} />
+			{isModalOpen && (
+				<ModalComponent closeModal={closeModal} addData={addData} />
+			)}
+		</>
+	);
 }
 
 export default App;
