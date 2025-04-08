6bcf5e26c8afeec220345fb312feb54ae489da45
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-04-08 14:09:14 +1000

    add sorting to amount and date

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,31 +1,77 @@
 import {
 	flexRender,
 	getCoreRowModel,
+	getFilteredRowModel,
+	getSortedRowModel,
 	useReactTable,
 } from '@tanstack/react-table';
+import { useState } from 'react';
 import '../styles/table.css';
 
 function TableComponent({ data = [] }) {
+	const [sorting, setSorting] = useState([]);
+
 	const columns = [
-		{ accessorKey: 'name', header: 'Name' },
-		{ accessorKey: 'type', header: 'Type' },
+		{
+			accessorKey: 'name',
+			header: 'Name',
+			enableSorting: false,
+			enableGlobalFilter: false, // disable global filtering for this column
+		},
+		{
+			accessorKey: 'type',
+			header: 'Type',
+			enableSorting: false,
+			enableColumnFilter: true,
+		},
 		{
 			accessorKey: 'amount',
 			header: 'Amount',
+			enableSorting: true,
+			enableGlobalFilter: false, // disable global filtering for this column
 			cell: (info) =>
 				`$${Number(info.getValue()).toLocaleString(undefined, {
 					minimumFractionDigits: 2,
 					maximumFractionDigits: 2,
 				})}`,
 		},
-		{ accessorKey: 'category', header: 'Category' },
-		{ accessorKey: 'date', header: 'Date' },
+		{
+			accessorKey: 'category',
+			header: 'Category',
+			enableSorting: false,
+			enableColumnFilter: true,
+		},
+		{
+			accessorKey: 'date',
+			header: 'Date',
+			enableSorting: true,
+			enableGlobalFilter: false, // disable global filtering for this column
+		},
 	];
 
 	const table = useReactTable({
-		data,
+		data: data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		// getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			sorting,
+			// globalFilter,
+			// columnFilters: [
+			// 	{ id: 'type', value: typeFilter },
+			// 	{ id: 'category', value: categoryFilter },
+			// ],
+		},
+		onSortingChange: setSorting,
+		// onGlobalFilterChange: setGlobalFilter,
+		// onColumnFiltersChange: (filters) => {
+		// 	const type = filters.find((item) => item.id === 'type')?.value || '';
+		// 	const category =
+		// 		filters.find((item) => item.id === 'category')?.value || '';
+		// 	setTypeFilter(type);
+		// 	setCategoryFilter(category);
+		// },
 	});
 
 	// Check if data is empty or undefined
@@ -46,55 +92,69 @@ function TableComponent({ data = [] }) {
 	};
 
 	return (
-		<div className="table-container">
-			<table className="table">
-				<thead>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map((header) => (
-								<th key={header.id}>
-									{flexRender(
-										header.column.columnDef.header,
-										header.getContext()
-									)}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
+		<>
+			<div className="table-container">
+				<table className="table">
+					<thead>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<tr key={headerGroup.id}>
+								{headerGroup.headers.map((header) => (
+									<th
+										key={header.id}
+										onClick={
+											header.column.getCanSort()
+												? header.column.getToggleSortingHandler()
+												: undefined
+										}
+										style={{
+											cursor: header.column.getCanSort()
+												? 'pointer'
+												: 'default',
+										}}
+									>
+										{flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
+										{header.column.getCanSort() && (
+											<span>
+												{header.column.getIsSorted() === 'asc'
+													? '🔼'
+													: header.column.getIsSorted() === 'desc'
+													? '🔽'
+													: ' 🔄'}
+											</span>
+										)}
+									</th>
+								))}
+							</tr>
+						))}
+					</thead>
 
-				<tbody>
-					{table.getRowModel().rows.map((row) => (
-						<tr key={row.id}>
-							{row.getVisibleCells().map((cell) => {
-								// Apply the class for "type" and "amount" columns only
-								const className = getRowClass(
-									cell.column.id,
-									row.original.type
-								);
-								return (
-									<td key={cell.id} className={className}>
-										{flexRender(cell.column.columnDef.cell, cell.getContext())}
-									</td>
-								);
-							})}
-						</tr>
-					))}
-				</tbody>
-
-				{/*<tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>*/}
-			</table>
-		</div>
+					<tbody>
+						{table.getRowModel().rows.map((row) => (
+							<tr key={row.id}>
+								{row.getVisibleCells().map((cell) => {
+									// Apply the class for "type" and "amount" columns only
+									const className = getRowClass(
+										cell.column.id,
+										row.original.type
+									);
+									return (
+										<td key={cell.id} className={className}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</td>
+									);
+								})}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</>
 	);
 }
 
